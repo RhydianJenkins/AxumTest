f5aae46e70854870ad37d65c127b62cbc2633206
Rhydian Jenkins <rhydian.jenkins@basekit.com>
2024-03-26 15:21:02 +0000

    Add /blogs and /blogs/id endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
 use anyhow::Result;
 use axum::routing::get;
+use axum::Extension;
+use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, Row};
 
-#[derive(Debug, FromRow)]
+#[derive(Debug, FromRow, Serialize, Deserialize)]
 struct BlogPost {
     id: i32,
     date: String,
     title: String,
     body: String,
     author: String,
-}
-
-impl StrConsumer for BlogPost {
-    fn consume(&mut self, buf: &str) {
-        <String as StrConsumer>::consume(&mut self.body, buf)
-    }
 }
 
 async fn get_connection_pool(url: &str) -> Result<sqlx::SqlitePool> {
@@ -93,16 +89,38 @@ async fn say_hello() -> &'static str {
     "Hello, world!"
 }
 
+async fn get_blog_posts_handler(
+    Extension(pool): Extension<sqlx::SqlitePool>,
+) -> axum::Json<Vec<BlogPost>> {
+    let posts = get_blog_posts(pool).await.unwrap();
+    axum::Json(posts)
+}
+
+async fn get_blog_post_handler(
+    Extension(pool): Extension<sqlx::SqlitePool>,
+    axum::extract::Path(id): axum::extract::Path<i32>,
+) -> axum::Json<BlogPost> {
+    let posts = get_blog_post(pool, id).await.unwrap();
+    axum::Json(posts)
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenvy::dotenv().ok();
+
     let database_url = std::env::var("DATABASE_URL")?;
     let listen_address = std::env::var("LISTEN_ADDRESS")?;
     let listener = tokio::net::TcpListener::bind(&listen_address).await?;
     let pool = get_connection_pool(&database_url).await?;
+
     run_migrations(pool.clone()).await?;
 
-    let app = axum::Router::new().route("/hello", get(say_hello));
+    let app = axum::Router::new()
+        .route("/hello", get(say_hello))
+        .route("/blogs", get(get_blog_posts_handler))
+        .route("/blogs/:id", get(get_blog_post_handler))
+        .layer(Extension(pool.clone()));
+
     axum::serve(listener, app).await?;
 
     Ok(())
