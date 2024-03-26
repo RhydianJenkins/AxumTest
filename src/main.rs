4fc63a1ad0273df51046ef1a901a3c4c2f45506a
Rhydian Jenkins <rhydian.jenkins@basekit.com>
2024-03-26 14:14:07 +0000

    Add env + crates

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
+    dotenvy::dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL")?;
+    println!("Connecting to: {database_url}");
     Ok(())
 }
