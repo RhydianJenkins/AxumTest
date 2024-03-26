72d9e3fc39dc6eb6f71f1de751f94c1a4238defd
Rhydian Jenkins <rhydian.jenkins@basekit.com>
2024-03-26 14:44:43 +0000

    Some initial data

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,47 @@
 use anyhow::Result;
+use sqlx::FromRow;
+
+#[derive(Debug, FromRow)]
+struct BlogPost {
+    id: i32,
+    date: String,
+    title: String,
+    body: String,
+    author: String,
+}
+
+async fn get_connection_pool(url: &str) -> Result<sqlx::SqlitePool> {
+    let connection_pool = sqlx::SqlitePool::connect(url).await?;
+
+    Ok(connection_pool)
+}
+
+async fn run_migrations(pool: sqlx::SqlitePool) -> Result<()> {
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
+    Ok(())
+}
+
+async fn get_blog_posts(pool: sqlx::SqlitePool) -> Result<Vec<BlogPost>> {
+    let posts = sqlx::query_as::<_, BlogPost>("SELECT * FROM blog_posts")
+        .fetch_all(&pool)
+        .await?;
+
+    Ok(posts)
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenvy::dotenv().ok();
+
     let database_url = std::env::var("DATABASE_URL")?;
-    println!("Connecting to: {database_url}");
+    let pool = get_connection_pool(&database_url).await?;
+
+    run_migrations(pool.clone()).await?;
+
+    let posts = get_blog_posts(pool.clone()).await?;
+
+    println!("{:?}", posts);
+
     Ok(())
 }
